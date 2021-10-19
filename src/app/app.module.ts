32cd9c8a2f8b1d0b162e4b729b938b8e6e9fe7a7
Alessandro Valenti <al.valenti@reply.it>
2021-10-19 12:02:38 +0200

    FIX: test platformBrowserDynamic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { UtilsService } from './service/utils.service';
 import { FormsModule } from '@angular/forms';
 import { TextTruncatePipe } from './pipe/text-truncate.pipe';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 @NgModule({
   declarations: [
@@ -25,3 +26,4 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+platformBrowserDynamic().bootstrapModule(AppModule)
